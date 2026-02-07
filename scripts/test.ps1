8a6d18c265eab7ee41cce1e327f9197edf7e6234
zybron <zybron@gmail.com>
2026-02-06 20:12:35 -0500

    Disable pytest plugin autoload for tests

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -9,4 +9,5 @@ if (-not (Test-Path ".venv")) {
 
 & .\.venv\Scripts\python -m pip install --upgrade "pip<23.2"
 & .\.venv\Scripts\python -m pip install -r requirements_test.txt
-& .\.venv\Scripts\python -m pytest -q -p no:pytest_socket
+$env:PYTEST_DISABLE_PLUGIN_AUTOLOAD = "1"
+& .\.venv\Scripts\python -m pytest -q -p pytest_asyncio -p pytest_homeassistant_custom_component
