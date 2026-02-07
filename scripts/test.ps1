f214a910ff5a5eecff793e8f9089890527f86323
zybron <zybron@gmail.com>
2026-02-06 20:07:29 -0500

    Adjust test config for HA harness

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -7,6 +7,6 @@ if (-not (Test-Path ".venv")) {
     python -m venv .venv
 }
 
-& .\.venv\Scripts\python -m pip install --upgrade pip
+& .\.venv\Scripts\python -m pip install --upgrade "pip<23.2"
 & .\.venv\Scripts\python -m pip install -r requirements_test.txt
 & .\.venv\Scripts\python -m pytest -q
